da30160bb4fb6357f8f45168db9173539e84ae51
Timofey06 <tima.loktionov@yandex.ru>
2022-12-17 20:45:01 +0300

    6

--- MyShop/PathManager.cs
+++ MyShop/PathManager.cs
@@ -4,5 +4,7 @@ namespace MyShop
     public static class PathManager
     {
         public static string ImageProductPass = @"images/product/";
+
+        public static string SessionCard = "SessionCart";
     }
 }
