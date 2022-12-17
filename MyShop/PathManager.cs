0fbf121b53329a641b362365351ecb9fc47ce0c7
Timofey06 <tima.loktionov@yandex.ru>
2022-12-17 20:45:22 +0300

    Revert "6"
    This reverts commit da30160bb4fb6357f8f45168db9173539e84ae51.

--- MyShop/PathManager.cs
+++ MyShop/PathManager.cs
@@ -4,7 +4,5 @@ namespace MyShop
     public static class PathManager
     {
         public static string ImageProductPass = @"images/product/";
-
-        public static string SessionCard = "SessionCart";
     }
 }
