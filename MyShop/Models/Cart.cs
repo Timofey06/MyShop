e58ea1e88d78fdbea3e1d124938549a7f3881d05
Timofey06 <tima.loktionov@yandex.ru>
2022-12-17 20:50:10 +0300

    Revert "Revert "6""
    This reverts commit 0fbf121b53329a641b362365351ecb9fc47ce0c7.

---
+++ MyShop/Models/Cart.cs
@@ -0,0 +1,7 @@
+﻿namespace MyShop.Models
+{
+    public class Cart
+    {
+        public int ProductId { get; set; }
+    }
+}
---
+++ MyShop/Models/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MyShop.Models.ViewModels
+{
+    public class DetailsViewModel
+    {
+        public Product Product { get; set; }
+        public bool IsInCart { get; set; }
+        public DetailsViewModel(Product product, bool isInCart)
+        {
+            Product = product;
+            IsInCart = isInCart;
+        }
+        public DetailsViewModel()
+        {
+
+        }
+
+    }
+}
